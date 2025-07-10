d61746689043a018be268910a1ab8506b1489c8c
unknown <edgarcfajardo@gmail.com>
2025-07-09 19:12:06 -0600

    change color bar

--- src/components/TobBar.tsx
+++ src/components/TobBar.tsx
@@ -1,6 +1,6 @@
 export const TopBar = () => {
     return (
-        <header className="bg-slate-900 text-white p-5 text-center">
+        <header className="bg-purple-900 text-white p-5 text-center">
             <h1>Mini proyectos</h1>
         </header>
     )
